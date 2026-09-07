e83f23fd638fc73ce24c52d91888191062951409
bhadresh-dahanuwala <bhadresh.dahanuwala@gmail.com>
2026-09-07 10:17:57 -0400

    feat(pipeline): implement intermediate layer transformations for complex derivations

---
+++ databricks/src/intermediate/addresses.py
@@ -0,0 +1,21 @@
+import dlt
+from pyspark.sql.functions import col
+
+@dlt.table(
+    name="addresses",
+    comment="Deduplicated list of physical addresses for the conformed dimension."
+)
+def intermediate_addresses():
+    customer_address = dlt.read("customer_address")
+    
+    return (
+        customer_address
+        .select(
+            col("line_1"),
+            col("line_2"),
+            col("city"),
+            col("state"),
+            col("zip")
+        )
+        .distinct()
+    )
